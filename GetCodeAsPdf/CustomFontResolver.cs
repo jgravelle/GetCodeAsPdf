a9add1f0827eff2e0499bbf6d13efe7d71d9e2e9
jgravelle <j@gravelle.us>
2023-11-12 08:18:15 -0600

    Add project files.

---
+++ GetCodeAsPdf/CustomFontResolver.cs
@@ -0,0 +1,38 @@
+﻿using PdfSharp.Fonts;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace GetCodeAsPdf
+{
+    public class CustomFontResolver : IFontResolver
+    {
+        public static string DefaultFontName => "Arial";
+
+        public byte[] GetFont(string faceName)
+        {
+            switch (faceName)
+            {
+                case "Arial":
+                    return LoadFontData("GetCodeAsPdf.arial.ttf"); // Replace with your actual namespace
+                default:
+#pragma warning disable CS8603 // Possible null reference return.
+                    return null;
+#pragma warning restore CS8603 // Possible null reference return.
+            }
+        }
+
+        public FontResolverInfo ResolveTypeface(string familyName, bool isBold, bool isItalic)
+        {
+            return new FontResolverInfo("Arial");
+        }
+
+        private static byte[] LoadFontData(string name)
+        {
+            using Stream? stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(name) ?? throw new FileNotFoundException($"Resource not found: {name}");
+            byte[] data = new byte[stream.Length];
+            stream.Read(data, 0, data.Length);
+            return data;
+        }
+    }
+}
---
+++ GetCodeAsPdf/Program.cs
@@ -0,0 +1,61 @@
+﻿using MigraDoc.DocumentObjectModel;
+using MigraDoc.Rendering;
+using PdfSharp.Fonts;
+using System.Text;
+
+namespace GetCodeAsPdf
+{
+    class Program
+    {
+        static void Main()
+        {
+            GlobalFontSettings.FontResolver = new CustomFontResolver();
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string? solutionFile = Directory.GetFiles(currentDirectory, "*.sln").FirstOrDefault();
+
+            if (solutionFile == null)
+            {
+                Console.WriteLine("No .sln file found in the current directory.");
+                return;
+            }
+
+            string solutionName = Path.GetFileNameWithoutExtension(solutionFile);
+            string outputFile = Path.Combine(currentDirectory, $"{solutionName}.pdf");
+
+            StringBuilder sb = new();
+            string[] fileExtensions = { "*.cs", "*.cshtml", "*.js", "*.css" }; // Add more if needed
+
+            foreach (string ext in fileExtensions)
+            {
+                foreach (string file in Directory.EnumerateFiles(currentDirectory, ext, SearchOption.AllDirectories))
+                {
+                    sb.AppendLine($"File: {file}");
+                    sb.AppendLine(File.ReadAllText(file));
+                    sb.AppendLine("\n\n"); // Adding some space between files
+                }
+            }
+
+            CreatePdf(sb.ToString(), outputFile);
+        }
+
+        static void CreatePdf(string content, string outputPath)
+        {
+            Document document = new();
+            Section section = document.AddSection();
+            Paragraph paragraph = section.AddParagraph();
+            paragraph.Format.Font.Name = "Arial";
+            paragraph.Format.Font.Size = 10;
+            paragraph.AddText(content);
+
+            PdfDocumentRenderer renderer = new()
+            {
+                Document = document
+            };
+            renderer.RenderDocument();
+            renderer.PdfDocument.Save(outputPath);
+
+            Console.WriteLine($"PDF saved to {outputPath}");
+        }
+    }
+}
